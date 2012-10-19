9cbd49ec57c056b8e4ca51188fbb180d39af2590
root <root@redwing.home>
2012-10-19 05:29:35 +0100

    Added git update script, couple of minor changes for the access point

--- access_point/start_access_point.sh
+++ access_point/start_access_point.sh
@@ -2,7 +2,7 @@
 killall hostapd
 clear
 
-USAGE="Usage: start_access_point <wlan> <device>        Where <wlan> is your access point wlan dongle and <device> is the working internet connection you want to bridge to (if it exists, otherwise just use eth0)"
+USAGE="Usage: start_access_point <wlan> <essid> <bridge_to>"
 if [ "$1" == "" ];then
 	echo $USAGE
 	exit 0
@@ -13,19 +13,35 @@ if [ "$2" == "" ];then
 	exit 0
 fi
 
-ifconfig $1 up 10.0.0.1 netmask 255.255.255.0
-udhcpd $1 &
+
+if [ "$3" == "" ];then
+	echo $USAGE
+	exit 0
+fi
+
+WLAN=$1
+ESSID=2
+BRIDGETO=$3
+
+ifconfig $WLAN down
+iwconfig $WLAN mode ad-hoc
+ifconfig $WLAN up
+iwconfig $WLAN essid "$ESSID"
+ifconfig $WLAN inet 10.0.0.1
+
+
+ifconfig $WLAN up 10.0.0.1 netmask 255.255.255.0
+udhcpd $WLAN &
 
 iptables --flush
 iptables --table nat --flush
 iptables --delete-chain
 iptables --table nat --delete-chain
-iptables --table nat --append POSTROUTING --out-interface $2 -j MASQUERADE
-iptables --append FORWARD --in-interface $1 -j ACCEPT
+iptables --table nat --append POSTROUTING --out-interface $BRIDGETO -j MASQUERADE
+iptables --append FORWARD --in-interface $WLAN -j ACCEPT
 sysctl -w net.ipv4.ip_forward=1
 
 # Now actually start hostapd
 hostapd /boot/redwing-pi/access_point/hostapd.conf
-# 1>/dev/null
-
+#hostapd /boot/redwing-pi/access_point/hostapd.conf 1> /dev/null &
 
